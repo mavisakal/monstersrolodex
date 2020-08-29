80f9d6ef9f71f567040f99d0ce0344c5c3db2f7a
tuncays <tuncayson@gmail.com>
2020-08-29 14:53:10 +0300

    Updates

---
+++ precache-manifest.ba5d43bd8add8f8c409e9fc13eff8474.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07c2adc1f34de74a81b6d94e77795dac",
+    "url": "/monstersrolodex/index.html"
+  },
+  {
+    "revision": "721136ec2e910590ea27",
+    "url": "/monstersrolodex/static/css/main.29f89f3c.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monstersrolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monstersrolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "721136ec2e910590ea27",
+    "url": "/monstersrolodex/static/js/main.34677492.chunk.js"
+  },
+  {
+    "revision": "902caa65de6c41620983",
+    "url": "/monstersrolodex/static/js/runtime-main.cc379c08.js"
+  }
+]);
